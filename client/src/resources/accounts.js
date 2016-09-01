66d48ae625972e4c67ae42641248d52ae6f06919
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-01 11:48:05 +0900

    Update ui base

---
+++ client/src/resources/accounts.js
@@ -0,0 +1,26 @@
+import request from 'axios';
+
+
+export class Accounts {
+  constructor() {
+    this._cache = [];
+    this._url = '/api/v1/accounts';
+  }
+  _url(id = null) {
+    if (id != null) {
+      return `${this._url}/${id}`;
+    } else {
+      return this._url;
+    }
+  }
+  index() {
+    return new Promise((resolve, reject) => {
+      request.get(this._url()).then((res) => {
+        this._cache = res;
+        resolve(res);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  }
+}
