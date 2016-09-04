d77f343bb903cf289bd849be013b7fb589539728
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-05 07:28:13 +0900

    Add validation to models/account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :user, class_name: 'User'
+
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
 end
