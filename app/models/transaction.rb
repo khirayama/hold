8fe404770fea8d4d182d72da65fdd40bff9bbe67
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-05 07:29:31 +0900

    Add validation to transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user, class_name: 'User'
+
+  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
+  validates :payment_date, presence: true
+  validates :transaction_date, presence: true
 end
