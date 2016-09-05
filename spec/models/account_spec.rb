7f61aea061ad50eee52b21f3a4b073ff7f089d74
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-06 07:00:51 +0900

    Add a test

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   describe "#name" do
-    context 'not empty' do
+    context 'not nil' do
+      let(:account) { Account.new(name: nil) }
+
       it 'not valid' do
-        account = Account.new(name: '')
+        account.valid?
+        expect(account.errors[:name]).to be_present
+      end
+    end
+
+    context 'not empty' do
+      let(:account) { Account.new(name: '') }
+
+      it 'not valid' do
         account.valid?
         expect(account.errors[:name]).to be_present
       end
