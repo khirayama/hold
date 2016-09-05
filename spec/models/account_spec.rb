7a04ff67760d999d76759eb3830249b6bdf2ba75
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-06 07:21:10 +0900

    Add Account#amount tests

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Account, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(50) }
   end
+
+  describe "#amount" do
+    it { should validate_presence_of(:amount) }
+    it { should validate_numericality_of(:amount).only_integer.is_greater_than_or_equal_to(0) }
+  end
 end
