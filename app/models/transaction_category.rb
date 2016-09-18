914a83f1742f743830d43c2b0e53fd3f9bebebc0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-18 14:44:44 +0900

    Add subscription to default transaction categories

--- app/models/transaction_category.rb
+++ app/models/transaction_category.rb
@@ -12,6 +12,7 @@ class TransactionCategory < ApplicationRecord
     user.transaction_categories.create({ name: I18n.t('transaction_categories.home'), transaction_type: :payment })
     user.transaction_categories.create({ name: I18n.t('transaction_categories.socializing'), transaction_type: :payment })
     user.transaction_categories.create({ name: I18n.t('transaction_categories.hobbies'), transaction_type: :payment })
+    user.transaction_categories.create({ name: I18n.t('transaction_categories.subscription'), transaction_type: :payment })
     user.transaction_categories.create({ name: I18n.t('transaction_categories.education'), transaction_type: :payment })
     user.transaction_categories.create({ name: I18n.t('transaction_categories.medical'), transaction_type: :payment })
     user.transaction_categories.create({ name: I18n.t('transaction_categories.other'), transaction_type: :payment })
