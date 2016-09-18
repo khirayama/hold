26d44ff186c221f0fb71c40307060f8f92119ab0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-18 16:43:18 +0900

    Fix migrate

--- db/migrate/20160826124854_create_accounts.rb
+++ db/migrate/20160826124854_create_accounts.rb
@@ -2,7 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :accounts do |t|
       t.integer :user_id, null: false
-      t.string :name
+      t.string :name, null: false
       t.float :amount, null: false
 
       t.timestamps
