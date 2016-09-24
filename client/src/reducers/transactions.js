552d97e0274a395e83cbb7b496cb7a9e22e1b5a2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-24 15:47:31 +0900

    Update transaction reducer

--- client/src/reducers/transactions.js
+++ client/src/reducers/transactions.js
@@ -1,10 +1,44 @@
 import types from '../constants/action-types';
 
 
+export function createTransaction(transactions, createdTransaction) {
+  const newTransactions = transactions.concat();
+
+  newTransactions.push(createdTransaction);
+  return newTransactions;
+}
+
+export function updateTransaction(transactions, updatedTransaction) {
+  return transactions.map((transaction) => {
+    if (transaction.cid === updatedTransaction.cid) {
+      return updatedTransaction;
+    }
+    return transaction;
+  });
+}
+
+export function deleteTransaction(transactions, deletedTransaction) {
+  return transactions.filter((transaction) => (
+    transaction.cid !== deletedTransaction.cid
+  ));
+}
+
 export default function transactionsReducer(state, action) {
   switch (action.type) {
     case types.FETCH_TRANSACTIONS:
       return action.transactions;
+    case types.CREATE_TRANSACTION:
+      return createTransaction(state, action.transaction);
+    case types.FAIL_TO_CREATE_TRANSACTION:
+      return updateTransaction(state, action.transaction);
+    case types.UPDATE_TRANSACTION:
+      return updateTransaction(state, action.transaction);
+    case types.FAIL_TO_UPDATE_TRANSACTION:
+      return updateTransaction(state, action.transaction);
+    case types.DELETE_TRANSACTION:
+      return deleteTransaction(state, action.transaction);
+    case types.FAIL_TO_DELETE_TRANSACTION:
+      return updateTransaction(state, action.transaction);
     default:
       return state.concat() || [];
   }
