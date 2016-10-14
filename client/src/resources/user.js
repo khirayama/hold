9c88dae6bd8841abc870cc70ebf0f529a3a63ac6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-10-14 17:12:56 +0900

    Create libs/micro-resource.js

--- client/src/resources/user.js
+++ client/src/resources/user.js
@@ -1,34 +1,10 @@
-import request from 'axios';
+import {EntryResource} from '../libs/micro-resource';
 
-export class UserModel {
+export class UserResource extends EntryResource {
   constructor() {
-    this._cache = null;
+    super();
     this._resourceUrl = '/api/v1/user';
-  }
-  _url(id = null) {
-    if (id !== null) {
-      return `${this._resourceUrl}/${id}`;
-    }
-    return this._resourceUrl;
-  }
-  get data() {
-    return this._cache;
-  }
-  fetch(cache = true) {
-    if (cache && this._cache !== null) {
-      return new Promise(resolve => {
-        resolve(this._cache);
-      });
-    }
-    return new Promise((resolve, reject) => {
-      request.get(this._url()).then(res => {
-        this._cache = res.data;
-        resolve(res.data);
-      }).catch(error => {
-        reject(error);
-      });
-    });
   }
 }
 
-export default new UserModel();
+export default new UserResource();
