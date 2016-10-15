44b6b3bbb93d40cab82f1ca70b14d1733b19b61b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-10-15 11:43:12 +0900

    Add formatUser test case

---
+++ client/src/actions/formatter.test.js
@@ -0,0 +1,79 @@
+import test from 'ava';
+
+import {
+  formatUser,
+  formatTransactionCategory,
+  formatAccount,
+  formatTransaction,
+} from './formatter';
+
+// formatUser
+test('formatUser > without user, setting and error', t => {
+  const user = {};
+  const setting = {};
+  const result = formatUser(user, setting);
+
+  t.deepEqual(result, {
+    cid: result.cid,
+    id: null,
+    name: '',
+    imageUrl: null,
+    error: null,
+    setting: {
+      language: undefined,
+      currencyCode: undefined,
+    },
+  });
+});
+
+test('formatUser > with user and setting without error', t => {
+  const user = {
+    cid: 'cid',
+    id: 1,
+    nickname: 'test name',
+    image_url: 'test url',
+  };
+  const setting = {
+    language: 'en',
+    currency_code: 'USD',
+  };
+  const result = formatUser(user, setting);
+
+  t.deepEqual(result, {
+    cid: 'cid',
+    id: 1,
+    name: 'test name',
+    imageUrl: 'test url',
+    error: null,
+    setting: {
+      language: 'en',
+      currencyCode: 'USD',
+    },
+  });
+});
+
+test('formatUser > with user, setting and error', t => {
+  const user = {
+    cid: 'cid',
+    id: 1,
+    nickname: 'test name',
+    image_url: 'test url',
+  };
+  const setting = {
+    language: 'en',
+    currency_code: 'USD',
+  };
+  const result = formatUser(user, setting, {message: 'error'});
+
+  t.deepEqual(result, {
+    cid: 'cid',
+    id: 1,
+    name: 'test name',
+    imageUrl: 'test url',
+    error: {message: 'error'},
+    setting: {
+      language: 'en',
+      currencyCode: 'USD',
+    },
+  });
+});
