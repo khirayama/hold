b84e983ae6177c37ff5048825d24d1498ac438e5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-10-27 06:33:10 +0900

    Change modal effect

--- client/src/views/desktop/components/modal.js
+++ client/src/views/desktop/components/modal.js
@@ -1,22 +1,31 @@
 import React, {Component} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import FlatButton from './flat-button';
 
 export default class Modal extends Component {
   render() {
     const isShown = this.props.isShown;
+    let content = null;
 
     if (isShown) {
-      return (
-        <div className="modal-mask">
-          <div className="modal">
-            <FlatButton onClick={this.props.onCloseButtonClick}>CLOSE</FlatButton>
-            {this.props.children}
+      content = (
+          <div className="modal-mask" key="modal-mask">
+            <div className="modal">
+              <FlatButton onClick={this.props.onCloseButtonClick}>CLOSE</FlatButton>
+              {this.props.children}
+            </div>
           </div>
-        </div>
       );
     }
-    return null;
+    return (
+      <ReactCSSTransitionGroup
+        className="modal-container"
+        transitionName="modal-transition"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}
+      >{content}</ReactCSSTransitionGroup>
+    );
   }
 }
 
