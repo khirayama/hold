dea9a4c55eef0a9a017048cdb684c26eb2d3acca
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-08 07:06:49 +0900

    Update mobile container

--- client/src/views/mobile/container.js
+++ client/src/views/mobile/container.js
@@ -1,8 +1,12 @@
 /* eslint-env browser */
 
 import React, {Component} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
+import {TRANSITION_TIME} from 'constants/constants';
 import {startMobileApp} from '../../actions/app-action-creators';
+
+import DashboardPage from './pages/dashboard-page';
 
 const propTypes = {
   store: React.PropTypes.object.isRequired,
@@ -12,9 +16,7 @@ export default class Container extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      store: this.props.store,
-    };
+    this.state = {store: this.props.store};
 
     this.updateState = this._updateState.bind(this);
   }
@@ -29,14 +31,33 @@ export default class Container extends Component {
   }
 
   _updateState() {
-    this.setState({
-      store: this.props.store,
-    });
+    this.setState({store: this.props.store});
+  }
+
+  _createPageElement(pathname, state) {
+    switch (pathname) {
+      case '/dashboard':
+        return <DashboardPage key="dashboard-page" state={state}/>;
+      default:
+        return <div key="not-fount-page">not found</div>;
+    }
   }
 
   render() {
+    const state = this.state.store.getState();
+    const pageElement = this._createPageElement(state.pathname, state);
+
+    if (!state.load) {
+      return null;
+    }
+
     return (
-      <div>Mobile App</div>
+      <ReactCSSTransitionGroup
+        className="page-container"
+        transitionName="page-transition"
+        transitionEnterTimeout={TRANSITION_TIME}
+        transitionLeaveTimeout={TRANSITION_TIME}
+        >{pageElement}</ReactCSSTransitionGroup>
     );
   }
 }
